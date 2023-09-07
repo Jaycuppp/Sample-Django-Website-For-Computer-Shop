f78b9e2502e50811c193738fed19e93d559d6a90
Hakob <hkeshis911@student.glendale.edu>
2023-09-07 03:20:41 -0700

    Add files via upload

--- Customers/forms.py
+++ Customers/forms.py
@@ -4,17 +4,28 @@ from django.contrib.auth.models import User
 from django import forms
 
 class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
-    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"class": "form-control"}))
-    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"class": "form-control"}))
-    
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        
         
     def __init__(self, *args: Any, **kwargs: Any):
         super(UserRegistrationForm, self).__init__(*args, **kwargs)
         
         self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['id'] = 'Registration_Username'
+        
+        self.fields['first_name'].widget.attrs['class'] = 'form-control'
+        self.fields['first_name'].widget.attrs['id'] = 'Registration_First_Name'
+        
+        self.fields['last_name'].widget.attrs['class'] = 'form-control'
+        self.fields['last_name'].widget.attrs['id'] = 'Registration_Last_Name'
+        
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['id'] = 'Registration_Email'
+        
         self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['id'] = 'Registration_Password1'
+        
         self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['id'] = 'Registration_Password2'
